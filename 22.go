6f18a34eae54b38749937826bf5ce0b2a95064d6
Gilang Wasis <100761068+langss1@users.noreply.github.com>
2024-06-07 22:20:00 +0700

    Update 22.go

--- 22.go
+++ 22.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-func decimalToOktal(n int) int {
+func decimalToBiner(n int) int {
 	if n == 0 {
 		return 0
 	} else {
-		return (n % 8) + decimalToOktal(n/8)*10
+		return (n % 2) + decimalToBiner(n/2)*10
 	}
 }
 
