66f0ebf65865f70b62d8a31bf68762cac3dec677
Gilang Wasis <100761068+langss1@users.noreply.github.com>
2024-06-08 11:56:00 +0700

    Update 18.go

--- 18.go
+++ 18.go
@@ -1,19 +1,11 @@
-package main
-
-import "fmt"
-
 func perkalianBilanganGenap(N int) int {
+/*  mengembalikan sebuah bilangan bulat yang menyatakan 
+    hasil perkalian bilangan genap dari 2 hingga N */
 	if N <= 0 {
 		return 1
 	}
-	if N%2 == 1 {
+	if N%2 == 0 {
 		return N * perkalianBilanganGenap(N-2)
 	}
 	return perkalianBilanganGenap(N - 1)
-}
-
-func main() {
-	var N int
-	fmt.Scan(&N)
-	fmt.Println(perkalianBilanganGenap(N))
 }
